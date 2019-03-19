23faaf6afec05384b8272a5cf176346b07cd1c2c
Sinthorn Pradutnam <psinthorn@gmail.com>
2019-03-19 23:39:44 +0700

    Version 1.0.0

--- config/key.js
+++ config/key.js
@@ -1,8 +1,8 @@
-module.exports = {
-    //mongoURI: 'mongodb://ecosyn:13579111315@ds247678.mlab.com:47678/apdlca',
-    mongoURI: 'mongodb://mscs:mscspets2018@ds233452.mlab.com:33452/mscspets',
-    //mongoURI: 'mongodb://localhost/apdlca',
-    googleClientID: '534700586866-vn91vc6t7bgqut7k07s0tc3o89dkkge2.apps.googleusercontent.com',
-    googleClientSecret: 'XqHaY9tcn-5gypYq6Bu6d1R3'
+const prod = require("./prod");
+const dev = require("./dev");
 
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = prod;
+} else {
+  module.exports = dev;
+}
---
+++ config/prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: process.env.MONGODB,
+  googleClientID: process.env.GOOGLE_ID,
+  googleClientSecret: process.env.GOOGLE_SECRET
+};
